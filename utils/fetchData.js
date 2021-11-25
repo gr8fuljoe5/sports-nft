274ac03427a6dc7d95d2d7eebb11532b832da12c
Joe Kanakaraj <jkanakaraj@gmail.com>
2021-11-24 19:26:28 -0500

    adding logs

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,5 +1,5 @@
 export default async function fetchData(URI) {
   const response = await fetch(URI);
-  console.log("fetchData", response);
+  console.log("fetchData", response.json());
   return await response.json();
 }
