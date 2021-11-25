35fc5c48451e8d1a720463842298774fe0a7cf6c
Joe Kanakaraj <jkanakaraj@gmail.com>
2021-11-25 09:10:10 -0500

    using SWR

--- pages/api/content/nft.js
+++ pages/api/content/nft.js
@@ -6,7 +6,8 @@ export default async function handler(req, res) {
     // This is the space ID. A space is like a project folder in Contentful terms
     space: process.env.CONTENTFUL_SPACE_ID,
     // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-    accessToken: process.env.CONTENTFUL_DELIVER_API_ACCESS_TOKEN,
+    accessToken: process.env.CONTENTFUL_PREVIEW_API_ACCESS_TOKEN,
+    host: "preview.contentful.com",
   });
   // This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
   try {
