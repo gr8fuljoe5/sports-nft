43c46f89bacab44f91e54353f76b864ec3f3e3a9
Joe Kanakaraj <jkanakaraj@gmail.com>
2021-11-26 10:51:45 -0500

    adding league info to homepage

---
+++ components/BottomNavigation/index.js
@@ -0,0 +1,35 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+
+export default function SimpleBottomNavigation(props) {
+  const [value, setValue] = React.useState(0);
+  console.log(props.data);
+  const { data } = props;
+  return (
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        {data &&
+          data.map((item, idx) => {
+            return (
+              <BottomNavigationAction
+                key={idx}
+                label={item.leagueInfo.league}
+                icon={<item.icon />}
+              />
+            );
+          })}
+      </BottomNavigation>
+    </Box>
+  );
+}
