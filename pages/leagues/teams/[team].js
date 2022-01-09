8b73e1436937f387e99448067ab1a043d146071f
Joe Kanakaraj <jkanakaraj@gmail.com>
2022-01-09 15:45:47 -0500

    #13 Clean up so build is working

--- pages/leagues/teams/[team].js
+++ pages/leagues/teams/[team].js
@@ -1,17 +1,17 @@
 import React from "react";
-import { getLeagueData } from "../../lib/leagues";
+// import { getLeagueData } from "../../lib/leagues";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 
-export async function getServerSideProps(context) {
-  console.log(context);
-  const league = await getLeagueData();
-  return {
-    props: {
-      league,
-    }, // will be passed to the page component as props
-  };
-}
+// export async function getServerSideProps(context) {
+//   console.log(context);
+//   const league = await getLeagueData();
+//   return {
+//     props: {
+//       league,
+//     }, // will be passed to the page component as props
+//   };
+// }
 
 const Team = (props) => {
   return (
