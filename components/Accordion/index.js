8256fc6dc098bee5395763e62767a7d586865846
Joe Kanakaraj <jkanakaraj@gmail.com>
2022-01-09 14:37:46 -0500

    adding league pages

---
+++ components/Accordion/index.js
@@ -0,0 +1,52 @@
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+
+export default function AccordionComponent(props) {
+  const { data } = props;
+  return (
+    <div>
+      {data &&
+        data.map((item) => {
+          console.log(item);
+          const { teams } = item;
+          return (
+            <Accordion key={item.league}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>{item.league}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <List dense={true}>
+                  {teams &&
+                    teams.map((item) => {
+                      return (
+                        <ListItem key={item.fields.name}>
+                          <ListItemText>{item.fields.name}</ListItemText>
+                        </ListItem>
+                      );
+                    })}
+                  {!teams && (
+                    <ListItem>
+                      <ListItemText>
+                        <Typography>No Teams available</Typography>
+                      </ListItemText>
+                    </ListItem>
+                  )}
+                </List>
+              </AccordionDetails>
+            </Accordion>
+          );
+        })}
+    </div>
+  );
+}
---
+++ pages/leagues/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { getLeagueData } from "../../lib/leagues";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Link from "next/link";
+import Accordion from "../../components/Accordion";
+
+export async function getServerSideProps(context) {
+  const leagues = await getLeagueData();
+  return {
+    props: {
+      leagues,
+    }, // will be passed to the page component as props
+  };
+}
+
+const Index = (props) => {
+  const { leagues } = props;
+  console.log("props", props);
+
+  return (
+    <section>
+      <Typography variant={"h1"}>Leagues</Typography>
+      <Grid container>
+        <Grid item>
+          <Accordion data={leagues} />
+        </Grid>
+      </Grid>
+    </section>
+  );
+};
+
+export default Index;
