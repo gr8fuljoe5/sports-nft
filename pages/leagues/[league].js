f4a37111f9330af5566d3adca2b7d4da44dd99f9
Joe Kanakaraj <jkanakaraj@gmail.com>
2022-01-09 15:22:00 -0500

    team display

--- pages/leagues/[league].js
+++ pages/leagues/[league].js
@@ -2,27 +2,36 @@ import React from "react";
 import { getLeagueData } from "../../lib/leagues";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import Link from "next/link";
-import Accordion from "../../components/Accordion";
 
 export async function getServerSideProps(context) {
-  const league = context.query.league.toUpperCase();
-  const leagues = await getLeagueData(league);
+  const sport = context.query.league.toUpperCase();
+  const league = await getLeagueData(sport);
   return {
     props: {
-      leagues,
+      league,
     }, // will be passed to the page component as props
   };
 }
 
 const League = (props) => {
+  const { league } = props;
+  const { league: leagueName, teams } = league[0].fields;
   return (
-    <section>
-      <Typography variant={"h1"}>Leagues</Typography>
-      <Grid container>
-        <Grid item>item</Grid>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography variant={"h1"}>{leagueName}</Typography>
+        <Grid container spacing={2}>
+          {teams &&
+            teams.map((item) => {
+              return (
+                <Grid item key={item.fields.name} xs={12} md={4}>
+                  {item.fields.name}
+                </Grid>
+              );
+            })}
+        </Grid>
       </Grid>
-    </section>
+    </Grid>
   );
 };
 
